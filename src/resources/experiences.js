44ff34c7b65725688828dacb5c44ebd02a98d8db
Deepika Pal <paldeepika639471@gmail.com>
2026-07-03 09:59:26 +0530

    1st commit

---
+++ src/resources/experiences.js
@@ -0,0 +1,16 @@
+export const experiences = [
+  {
+    id:"1",
+    company:"Tech Mahindra",
+    title:"Customer Support Associate",
+    period:"2025-2026",
+    description:"I worked as a Customer Support Associate at Tech Mahindra, where I provided assistance to customers and resolved their queries efficiently."
+  },
+   {
+    id:"2",
+    company:"Tech Mahindra",
+    title:"Customer Support Associate",
+    period:"2025-2026",
+    description:"I worked as a Customer Support Associate at Tech Mahindra, where I provided assistance to customers and resolved their queries efficiently."
+  }
+];
