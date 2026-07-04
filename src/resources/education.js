2d97970c563085d3df3f5326d4e98c511437cbaa
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-04 21:31:08 +0530

    updated file

---
+++ src/resources/education.js
@@ -0,0 +1,23 @@
+export const education= [
+  {
+    id:"1",
+    college:"Maharana Pratap Engineering College, Kanpur",
+    degree:"Master of  Computer Application",
+    period:"2023-2025",
+    percentage:"8.32 CGPA",
+  },
+  {
+     id:"2",
+    college:"CSJM University, Kanpur",
+    degree:"Bachelor of Computer Application",
+    period:"2020-2023",
+    percentage:"7.5 CGPA",
+  },
+  {
+     id:"3",
+    college:"SGM Inter College, Kanpur",
+    degree:"Intermediate",
+    period:"2019-2020",
+    percentage:"73%",
+  },
+];
---
+++ src/resources/project.js
@@ -0,0 +1,34 @@
+export const project =[
+  {
+    technologies:"",
+    id:"1",
+    title:"portfolio",
+    image:"",
+    description:"Hi! I'm a passionate Computer Science student with a strong interes",
+    link:"/",
+  },
+  {
+    technologies:"",
+    id:"2",
+    title:"Landing Page",
+    image:"",
+    description:"Hi! I'm a passionate Computer Science student with a strong interes",
+    link:"/",
+  },
+  {
+    technologies:"",
+    id:"3",
+    title:"Calculator",
+    image:"https://i.ytimg.com/vi/Ng0zgEtIou8/maxresdefault.jpg",
+    description:"Hi! I'm a passionate Computer Science student with a strong interes",
+    link:"/",
+  },
+  {
+    technologies:"",
+    id:"4",
+    title:"File converter",
+    image:"",
+    description:"Hi! I'm a passionate Computer Science student with a strong interes",
+    link:"/",
+  },
+]
