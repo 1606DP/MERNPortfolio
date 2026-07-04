8ef385c63ce5701024e161e7f8e14789aa2f97e1
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-04 21:35:43 +0530

    update file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Loader from "./components/Loader";
+import { useState } from "react";
 
 function App() {
+  const [showLoading , setShowLoading] = useState(false);
   return (
     <BrowserRouter>
+    {showLoading ? <Loader /> : null}
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
